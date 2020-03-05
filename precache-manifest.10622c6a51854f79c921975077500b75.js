5ab532b4901329f381a0caa2079cae5e999465cf
adipinhas <adi9247@gmail.com>
2020-03-05 02:00:35 +0200

    deploy

---
+++ precache-manifest.10622c6a51854f79c921975077500b75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4cb64480751d7ef81a",
+    "url": "/testapp/css/app.b842baab.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/testapp/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2c1493fa81f3b389b80c32feca1e89f7",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "a6d57a5ea8ac6d7b72c2",
+    "url": "/testapp/js/about.916c2f00.js"
+  },
+  {
+    "revision": "fb4cb64480751d7ef81a",
+    "url": "/testapp/js/app.d0de6969.js"
+  },
+  {
+    "revision": "eca94e013a19b65867e7",
+    "url": "/testapp/js/chunk-vendors.8d57be69.js"
+  },
+  {
+    "revision": "cf1ec32d419f4d2d9fe930d7d36db7ad",
+    "url": "/testapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testapp/robots.txt"
+  }
+]);
